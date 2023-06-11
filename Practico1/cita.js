646289be7bf283fe968b7fb0b7a56c4bc80230f1
Agustín Perez <agustin0914@gmail.com>
2023-06-11 20:22:24 -0300

    Arreglo error en cita.js

--- Practico1/cita.js
+++ Practico1/cita.js
@@ -4,8 +4,9 @@ let substring = "tigres comen trigo";
 let tamañoDeCita = cita.length;
 console.log("El tamaño de la cita es: " + tamañoDeCita);
 
-let citaRevizada  = cita.split(" ");
 let indice = cita.indexOf(substring);
 console.log("El indice del substring es: " + indice);
-let Recorte = citaRevizada.splice(5,8);
+
+let citaArreglo  = cita.split(" ");
+let citaRevisada = citaArreglo.splice(0,5);
 console.log(citaRevisada);
